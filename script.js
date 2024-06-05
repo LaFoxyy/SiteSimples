9c7208e8b3c186e305530ae58cecd72a505d5953
LaFoxyy <dr4caryslolpro@gmail.com>
2024-06-05 13:00:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('ingressar-btn').addEventListener('click', function(){
+    window.location.href = 'https://chat.whatsapp.com/EVwUkJqrKqPLoQOu40a80l';
+})
+
+document.getElementById('banner-img').addEventListener('click', function(){
+    window.location.href = 'https://chat.whatsapp.com/EVwUkJqrKqPLoQOu40a80l';
+})
