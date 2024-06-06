c5f68f950ca82838ec27b17ca004353a5c95c2c3
LaFoxyy <dr4caryslolpro@gmail.com>
2024-06-06 18:51:10 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('ingressar-btn').addEventListener('click', function(){
-    window.location.href = 'https://api.whatsapp.com/send?phone=558596754008&text=Teste';
-})
-
-document.getElementById('banner-img').addEventListener('click', function(){
-    window.location.href = 'https://api.whatsapp.com/send?phone=558596754008&text=Teste';
-})
