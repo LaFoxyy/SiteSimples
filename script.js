e3320ce180a0a8f335ab668d530b887cf0b9624e
LaFoxyy <dr4caryslolpro@gmail.com>
2024-06-05 23:06:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 document.getElementById('ingressar-btn').addEventListener('click', function(){
     window.location.href = 'https://api.whatsapp.com/send?phone=558596754008&text=Teste';
-    
-    setTimeout(function(){
-        window.location.href = 'https://chat.whatsapp.com/EVwUkJqrKqPLoQOu40a80l';
-    }, 10000);
 })
 
 document.getElementById('banner-img').addEventListener('click', function(){
