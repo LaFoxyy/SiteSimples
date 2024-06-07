9ada7d7b3d973b7dec3402a8ab9246f7495b407e
LaFoxyy <dr4caryslolpro@gmail.com>
2024-06-07 14:28:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,16 +14,45 @@ document.addEventListener('DOMContentLoaded', function() {
         return true;
     }
 
+    function sendWebhook(callback) {
+        const name = document.getElementById('name').value;
+        const phone = document.getElementById('tel').value;
+
+        fetch('https://gcamancio.app.n8n.cloud/webhook-test/af7f3bf8-a2d5-438f-8108-582373498c4f', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: name,
+                phone: phone
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            callback();  // Call the callback function after successful webhook
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            callback();  // Call the callback function even if there's an error
+        });
+    }
+
     submitButton.addEventListener('click', function() {
         if (validateForm()) {
-            window.location.href = 'https://chat.whatsapp.com/GMe7MCeWc8Z2rS9AeSK4Nk';
+            sendWebhook(function() {
+                window.location.href = 'https://chat.whatsapp.com/GMe7MCeWc8Z2rS9AeSK4Nk';
+            });
         }
     });
 
     clickableImage.addEventListener('click', function(e) {
         e.preventDefault();
         if (validateForm()) {
-            window.location.href = 'https://chat.whatsapp.com/GMe7MCeWc8Z2rS9AeSK4Nk';
+            sendWebhook(function() {
+                window.location.href = 'https://chat.whatsapp.com/GMe7MCeWc8Z2rS9AeSK4Nk';
+            });
         }
     });
 });
